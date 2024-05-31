f41917171910babe8e855068535503b1fd5ec90a
farkasszili <farkasszili32@gmail.com>
2024-05-31 10:42:16 +0200

    Modify folder

---
+++ Python/2024-05-30/igen.py
@@ -0,0 +1,14 @@
+
+kecske = 2
+if (kecske == 1):
+    print("Kecske = 1-el")
+else:
+    print("A kecske nem egyenlő 1-el")
+
+also = 26
+if (also == 26):
+    print("Alsó = 26-al")
+else:
+    print("Az also nem egyenlő 26-al")
+
+
--- temp/ÁAV-paros.py
+++
@@ -1,6 +0,0 @@
-bekert_szam = int(input("Adj meg egy egész számot!"))
-if bekert_szam % 2 ==0:
-      print("A megadott szám páros.")
-else:
-      print("A megadott szám páratlan.")
-
